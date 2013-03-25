29d3cadbad1c5e7cb20a402ebbaa4b6bb53ad222
Pat Allan <pat@freelancing-gods.com>
2013-03-25 20:24:58 +1100

    Simplify OAuth setup - unlikely that we'll need to handle GitHub Enterprise again.

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,6 @@ module Gyoza
 
     config.middleware.use OmniAuth::Builder do
       provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_SECRET'],
-        client_options: {
-          site:          "#{Gyoza::GITHUB_HOST}/api/v3",
-          authorize_url: "#{Gyoza::GITHUB_HOST}/login/oauth/authorize",
-          token_url:     "#{Gyoza::GITHUB_HOST}/login/oauth/access_token"
-        },
         scope: 'user:email'
     end
   end
