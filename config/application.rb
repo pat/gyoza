a6b2ed60f941f7613c3a76097fcb7962772a5761
Pat Allan <pat@freelancing-gods.com>
2013-03-25 16:46:51 +1300

    Removing boilerplate comments, fixing OAuth configuration syntax.

--- config/application.rb
+++ config/application.rb
@@ -14,20 +14,13 @@ module Gyoza
   class Application < Rails::Application
     require 'gyoza'
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Melbourne'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     config.middleware.use OmniAuth::Builder do
-      provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_SECRET'],{
+      provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_SECRET'],
         client_options: {
           site:          "#{Gyoza::GITHUB_HOST}/api/v3",
           authorize_url: "#{Gyoza::GITHUB_HOST}/login/oauth/authorize",
