1d3ebb9f02fd30582fc109c9a3a5c5334a531198
Pat Allan <pat@freelancing-gods.com>
2013-03-25 17:04:16 +1300

    A home page with basic information.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 require 'sidekiq/web'
 
 Gyoza::Application.routes.draw do
+  root to: 'home#index'
+
   scope constraints: {path: /.*/, format: //} do
-    get '/edit/:user/:repo/*path' => 'sites#show'
-    put '/edit/:user/:repo/*path' => 'sites#update'
+    get '/edit/:user/:repo/*path' => 'sites#show',   as: :site_show
+    put '/edit/:user/:repo/*path' => 'sites#update', as: :site_update
   end
 
   get '/auth/:provider/callback', to: 'sessions#create'
