3abe3f59e36ea5e0522777eda4cfcb5fc3b1b895
Pat Allan <pat@freelancing-gods.com>
2013-03-25 16:33:03 +1300

    Some of a new Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gyoza::Application.config.secret_key_base = 'fe12686c30c3c7cc096e50d02ec728dc2d0aa01e50bb5f34936fdc8cb2d320dd00a93f57426182af6b1d1da2ff3f1beec99995c08cc1d012b1bf3d48c4a71a79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gyoza::Application.config.session_store :encrypted_cookie_store, key: '_gyoza_session'
