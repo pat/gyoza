6693cc55b4ac4adc736666b7422ec47c10cb5367
Pat Allan <pat@freelancing-gods.com>
2014-05-07 20:32:59 +1000

    Updating session store for Rails 4 proper.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Gyoza::Application.config.session_store :encrypted_cookie_store, key: '_gyoza_session'
+Gyoza::Application.config.session_store :cookie_store, key: '_gyoza_session'
